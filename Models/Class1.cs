89e8c9567b0b44b058fd5f4d2e52694660103a1b
Osama <osamaashkanani@gmail.com>
2024-12-24 15:16:12 +0300

    v0

---
+++ Models/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleT8b.Models
+{
+    public class Class1
+    {
+        public float Add(float a, float b)
+        {
+            return a + b;
+        }
+        public float Add(float a, float b, float c)
+        {
+            return a + b + c;
+        }
+        public float Add(float a, float b, string c)
+        {
+            float d = float.Parse(c);
+            return a + b + d;
+        }
+        public float Add(float salary, string bonus)
+        {
+            const int rate = 10;
+            return salary + (float.Parse(bonus) * rate);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleT8b.Models;
+
+Class1 fun = new Class1();
+
+Console.Write("Enter number 1: ");
+float a = float.Parse(Console.ReadLine());
+
+Console.Write("Enter number 2: ");
+float b = float.Parse(Console.ReadLine());
+
+Console.Write("Total= " + fun.Add(a, b));
+
+//________________________________________________________________________________________
+Console.Write("\n\nEnter number 1: ");
+float a2 = float.Parse(Console.ReadLine());
+
+Console.Write("Enter number 2: ");
+float b2 = float.Parse(Console.ReadLine());
+
+Console.Write("Enter number 3: ");
+float c2 = float.Parse(Console.ReadLine());
+
+Console.Write("Total= " + fun.Add(a2, b2, c2));
+
+//________________________________________________________________________________________
+Console.Write("\n\nEnter number 1: ");
+float a3 = float.Parse(Console.ReadLine());
+
+Console.Write("Enter number 2: ");
+float b3 = float.Parse(Console.ReadLine());
+
+Console.Write("Enter number 3: ");
+string c3 = Console.ReadLine();
+
+Console.Write("Total= " + fun.Add(a3, b3, c3));
+
+//________________________________________________________________________________________
+Console.Write("\n\nSalary: ");
+float a4 = float.Parse(Console.ReadLine());
+
+Console.Write("Working hours: ");
+string b4 = Console.ReadLine();
+
+Console.Write("Total Salary with bounce= " + fun.Add(a4, b4));
